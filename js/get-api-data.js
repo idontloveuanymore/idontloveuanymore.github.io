7782ad00130299f94bb5e814729d9c11466825f9
fade.awway <142530363+idontloveuanymore@users.noreply.github.com>
2024-02-26 21:48:38 +0400

    all?
    help

---
+++ js/get-api-data.js
@@ -0,0 +1,48 @@
+function updateMusicData() { 
+    fetch('https://qqq-aawdddsssaassss.amvera.io/') 
+      .then(response => response.json()) 
+      .then(data => { 
+        if (data.scrobbling_state !== null && data.play_url !== null) { 
+          document.getElementById('track-cover').src = data.image_url;
+          document.getElementById('track-cover').style.width = '70px';
+          document.getElementById('track-cover').style.height = '70px';
+          document.getElementById('track-name').innerText = data.track_name; 
+          document.getElementById('track-author').innerText = data.artist_name;
+          document.getElementById("audioGif").querySelector("img").src = "https://www.last.fm/static/images/icons/now_playing_grey_12.b4158f8790d0.gif";
+          document.getElementById('track-name').addEventListener('click', function() { 
+            window.location.href =  data.play_url; 
+          }); 
+        } else if (data.play_url == null) { 
+          document.getElementById('track-cover').src = data.image_url;
+          document.getElementById('track-cover').style.width = '70px';
+          document.getElementById('track-cover').style.height = '70px';
+          document.getElementById('track-name').innerText = data.track_name; 
+          document.getElementById('track-author').innerText = data.artist_name; 
+          document.getElementById("audioGif").querySelector("img").src = "https://www.last.fm/static/images/icons/now_playing_grey_12.b4158f8790d0.gif";
+          document.getElementById('track-name').addEventListener('click', function() { 
+            window.location.href =  'https://www.last.fm/user/fade_awway'; 
+          }); 
+        } else { 
+          document.getElementById('track-cover').src = ''; 
+          document.getElementById('track-name').innerText = 'More music'; 
+          document.getElementById('track-author').innerText = "All the tracks I've listened to"; 
+          document.getElementById("audioGif").querySelector("img").src = "https://i.ibb.co/wBtqcq0/inactive.png";
+          document.getElementById('track-name').addEventListener('click', function() { 
+            window.location.href = 'https://www.last.fm/user/fade_awway'; 
+          }); 
+        } 
+      }) 
+      .catch(error => { 
+        console.error('Error:', error);
+        document.getElementById('track-cover').src = ''; 
+        document.getElementById('track-name').innerText = 'More music'; 
+        document.getElementById('track-author').innerText = "All the tracks I've listened to"; 
+        document.getElementById("audioGif").querySelector("img").src = "https://i.ibb.co/wBtqcq0/inactive.png";
+        document.getElementById('track-name').addEventListener('click', function() { 
+        window.location.href = 'https://www.last.fm/user/fade_awway'; 
+        }); 
+      });
+  }
+  
+  setInterval(updateMusicData, 10000); 
+  updateMusicData();
