7efc9518f3ad9ad8cb6b964adc3416e35093016c
idontloveuanymore <aawdddsssaassss@gmail.com>
2024-05-14 20:06:36 +0400

    update music bar

--- js/get-api-data.js
+++ js/get-api-data.js
@@ -2,55 +2,45 @@
 const lastfmProfile = 'https://www.last.fm/user/fade_awway';
 const lastfmaApi = 'https://idontloveuanymore-lastfm-api.vercel.app/';
 
-function updateMusicData() { 
-    fetch(lastfmaApi) 
-      .then(response => response.json()) 
-      .then(data => { 
-        if (data.scrobbling_state !== null && data.play_url !== null) { 
-          document.getElementById('track-cover').src = data.image_url;
-          document.getElementById('track-cover').style.width = '70px';
-          document.getElementById('track-cover').style.height = '70px';
-          document.getElementById('track-name').innerText = data.track_name; 
-          document.getElementById('track-author').innerText = data.artist_name;
-          document.getElementById("audioGif").querySelector("img").src = "https://www.last.fm/static/images/icons/now_playing_grey_12.b4158f8790d0.gif";
-          document.getElementById('track-name').addEventListener('click', function() { 
-            window.location.href =  data.play_url; 
-          }); 
-        } else if (data.play_url == null) { 
-          document.getElementById('track-cover').src = data.image_url;
-          document.getElementById('track-cover').style.width = '70px';
-          document.getElementById('track-cover').style.height = '70px';
-          document.getElementById('track-name').innerText = data.track_name; 
-          document.getElementById('track-author').innerText = data.artist_name; 
-          document.getElementById("audioGif").querySelector("img").src = "https://www.last.fm/static/images/icons/now_playing_grey_12.b4158f8790d0.gif";
-          document.getElementById('track-name').addEventListener('click', function() { 
-            window.location.href = lastfmProfile; 
-          }); 
-        } else { 
-          document.getElementById('track-cover').src = '';
-          document.getElementById('track-cover').style.width = '0px';
-          document.getElementById('track-cover').style.height = '0px';
-          document.getElementById('track-name').innerText = 'More music'; 
-          document.getElementById('track-author').innerText = "All the tracks I've listened to"; 
-          document.getElementById("audioGif").querySelector("img").src = "https://i.ibb.co/wBtqcq0/inactive.png";
-          document.getElementById('track-name').addEventListener('click', function() { 
-            window.location.href = lastfmProfile; 
-          }); 
-        } 
-      }) 
-      .catch(error => { 
-        console.error('Error:', error);
-        document.getElementById('track-cover').src = '';
-        document.getElementById('track-cover').style.width = '0px';
-        document.getElementById('track-cover').style.height = '0px';
-        document.getElementById('track-name').innerText = 'More music'; 
-        document.getElementById('track-author').innerText = "All the tracks I've listened to"; 
-        document.getElementById("audioGif").querySelector("img").src = "https://i.ibb.co/wBtqcq0/inactive.png";
-        document.getElementById('track-name').addEventListener('click', function() { 
-        window.location.href = lastfmProfile; 
-        }); 
-      });
-  }
-  
-  setInterval(updateMusicData, 10000); 
-  updateMusicData();
+function updateTrackInfo(data) {
+  document.getElementById('track-name').style.paddingTop = "0px";
+  document.getElementById('track-cover').src = data.image_url;
+  document.getElementById('track-cover').style.display = "flex";
+  document.getElementById('track-name').innerText = data.track_name;
+  document.getElementById('track-author').innerText = data.artist_name;
+  document.getElementById("audioGif").querySelector("img").style.display = "flex";
+  document.getElementById('track-name').addEventListener('click', function() {
+    window.location.href = data.play_url ? data.play_url : lastfmProfile;
+  });
+}
+
+function updateInactiveState() {
+  document.getElementById('track-cover').style.display = "none";
+  document.getElementById('track-name').style.paddingTop = "5px";
+  document.getElementById('track-name').innerText = 'More music';
+  document.getElementById('track-author').innerText = "All the tracks I've listened to";
+  document.getElementById("audioGif").querySelector("img").style.display = "none";
+  document.getElementById('track-name').addEventListener('click', function() {
+    window.location.href = lastfmProfile;
+  });
+}
+
+function updateMusicData() {
+  fetch(lastfmaApi)
+    .then(response => response.json())
+    .then(data => {
+      if (data == null || data.scrobbling_state == null) {
+        updateInactiveState();
+      }
+      else {
+        updateTrackInfo(data);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      updateInactiveState();
+    });
+}
+
+setInterval(updateMusicData, 10000);
+updateMusicData();
